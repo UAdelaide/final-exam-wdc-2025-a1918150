1d4a9bdc972a32bd00d06023425faf2e9a0f13c9
Kevan Ghorecha <a1918150@adelaide.edu.au>
2025-06-20 10:56:30 +0000

    [automatic commit] 2025-06-20 10:56:30 UTC 14.203.77.138

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -97,7 +97,7 @@ router.get('/dogs', async (req, res) => {
     }
 });
 
-router.get('/dogs', async (req, res) => {
+router.get('/walkrequests/open', async (req, res) => {
     try {
         const [dogs] = await db.execute('SELECT d.name AS dog_name, d.size, u.username AS owner_username FROM Dogs AS d JOIN Users AS u ON d.owner_id = u.user_id');
         res.json(dogs);
