eeb8d42d2a2b98f6704b2eccb07fdc01d2931a7c
Kevan Ghorecha <a1918150@adelaide.edu.au>
2025-06-20 11:23:25 +0000

    [automatic commit] 2025-06-20 11:23:25 UTC 14.203.77.138

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -99,8 +99,8 @@ router.get('/dogs', async (req, res) => {
 
 router.get('/walkrequests/open', async (req, res) => {
     try {
-        const [dogs] = await db.execute('SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, owner_username FROM WalkRequests AS wr JOIN Dogs AS d ON wr.dog_id = d.dog_id JOIN Users AS u ON d.owner_id = u.user_id WHERE wr.status = "open"');
-        res.json(dogs);
+        const [wr] = await db.execute('SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, owner_username FROM WalkRequests AS wr JOIN Dogs AS d ON wr.dog_id = d.dog_id JOIN Users AS u ON d.owner_id = u.user_id WHERE wr.status = "open"');
+        res.json(wr);
     } catch (error) {
         console.error('Error fetching dogs:', error);
         res.status(500).json({ error: 'Failed to fetch dogs' });
