ca3d8cce82a22af49dc779e43adc7c604e8882e9
Kevan Ghorecha <a1918150@adelaide.edu.au>
2025-06-20 10:23:04 +0000

    [automatic commit] 2025-06-20 10:23:04 UTC 14.203.77.138

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -89,7 +89,7 @@ let db;
 // Route to return books as JSON
 router.get('/dogs', async (req, res) => {
     try {
-        const [dogs] = await db.execute('SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username FROM Dogs JOINS Users ON Dogs.owner_id = Users.user_id');
+        const [dogs] = await db.execute('SELECT d.name AS dog_name, d.size, u.username AS owner_username FROM Dogs AS d JOINS Users AS u ON Dogs.owner_id = Users.user_id');
         res.json(dogs);
     } catch (error) {
         console.error('Error fetching dogs:', error);
