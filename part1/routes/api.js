fcab4cc2f0e5a65035d3882ec578f32298bde5bb
Kevan Ghorecha <a1918150@adelaide.edu.au>
2025-06-20 10:02:00 +0000

    [automatic commit] 2025-06-20 10:02:00 UTC 14.203.77.138

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -88,12 +88,7 @@ let db;
 
 // Route to return books as JSON
 router.get('/', async (req, res) => {
-  try {
-    const [books] = await db.execute('SELECT * FROM books');
-    res.json(books);
-  } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch books' });
-  }
+  
 });
 
 module.exports = router;
