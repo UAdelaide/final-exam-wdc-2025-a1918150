f9955b57b07ac017e20231b42c435074f592fe6d
Kevan Ghorecha <a1918150@adelaide.edu.au>
2025-06-20 12:05:22 +0000

    [automatic commit] 2025-06-20 12:05:22 UTC 14.203.77.138

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -115,7 +115,7 @@ router.get('/walkrequests/open', async (req, res) => {
 
 router.get('/walkers/summary', async (req, res) => {
     try {
-        const [ws] = await db.execute(`SELECT u.username AS walker_username, COUNT(r.rating) AS total_ratings, ROUND(AVG(r.rating),1) AS average_rating, COUNT(w.request_id) AS completed_walks
+        const [ws] = await db.execute(`SELECT u.username AS walker_username, COUNT(r.rating) AS total_ratings, ROUND(AVG(r.rating), 1) AS average_rating, COUNT(w.request_id) AS completed_walks
                                         FROM Users AS u
                                         LEFT JOIN WalkRatings AS r ON u.user_id = r.walker_id
                                         LEFT JOIN WalkRequests AS w ON w.request_id = r.request_id AND w.status = "completed"
