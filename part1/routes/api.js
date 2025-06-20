a60dbd59334075fb2d920e395744048bde583e34
Kevan Ghorecha <a1918150@adelaide.edu.au>
2025-06-20 11:27:13 +0000

    [automatic commit] 2025-06-20 11:27:13 UTC 14.203.77.138

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -99,7 +99,7 @@ router.get('/dogs', async (req, res) => {
 
 router.get('/walkrequests/open', async (req, res) => {
     try {
-        const [wr] = await db.execute('SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, owner_username FROM WalkRequests AS wr JOIN Dogs AS d ON wr.dog_id = d.dog_id JOIN Users AS u ON d.owner_id = u.user_id WHERE wr.status = "open"');
+        const [wr] = await db.execute('SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.owner_username FROM WalkRequests AS wr JOIN Dogs AS d ON wr.dog_id = d.dog_id JOIN Users AS u ON d.owner_id = u.user_id WHERE wr.status = "open"');
         res.json(wr);
     } catch (error) {
         console.error('Error fetching walkrequests which are open:', error);
