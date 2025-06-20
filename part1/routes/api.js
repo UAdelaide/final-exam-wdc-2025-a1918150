611a1f2c614d897277346bb4b06709f6c4019070
Kevan Ghorecha <a1918150@adelaide.edu.au>
2025-06-20 11:46:36 +0000

    [automatic commit] 2025-06-20 11:46:36 UTC 14.203.77.138

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -117,7 +117,7 @@ router.get('/walkers/summary', async (req, res) => {
     try {
         const [ws] = await db.execute(`SELECT walker_username, total_ratings, average_rating, completed_walks
                                         FROM Users AS u
-                                        JOIN Dogs AS d ON wr.dog_id = d.dog_id
+                                        JOIN WalkRai AS d ON wr.dog_id = d.dog_id
                                         JOIN Users AS u ON d.owner_id = u.user_id
                                         WHERE wr.status = "open"`);
         res.json(ws);
