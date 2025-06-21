b0a0bd8ae67a0da883c50d53e3ae27b8f814e85f
Kevan Ghorecha <a1918150@adelaide.edu.au>
2025-06-21 05:15:38 +0000

    [automatic commit] 2025-06-21 05:15:38 UTC 220.244.146.92

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -63,7 +63,7 @@ router.post('/login', async (req, res) => {
 
 // New logout route to destroy session
 router.post('/logout', async (req, res) => {
-  req.session.destroy(err => {
+  req.session.destroy((err) => {
     if (err) {
       return res.status(500).json({ error: 'Logout failed' });
     }
